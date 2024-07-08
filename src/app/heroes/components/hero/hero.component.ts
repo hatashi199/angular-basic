04f17092e755a88251533c38dbc787ed8b0bd07b
Alejandro Mariño <alejandromf_199@hotmail.com>
2024-07-08 21:47:37 +0200

    v1.0.0 => Directivas y Modulos de angular

---
+++ src/app/heroes/components/hero/hero.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+    public name: string = 'ironman';
+    public age: number = 45;
+
+    get capitalizedNamed(): string {
+        return this.name.toUpperCase();
+    }
+
+    getHeroDescription(): string {
+        return `${this.name} - ${this.age} años`
+    }
+
+    changeHero(newHero: string):void {
+        this.name = newHero;
+    }
+
+    changeAge(newAge: number):void {
+        this.age = newAge
+    }
+
+    resetAll():void {
+        this.name = 'ironman';
+        this.age = 45;
+    }
+}
