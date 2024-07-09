88f97693884ab883ea3a285216dd3d1c7b022e33
Alejandro Mariño <alejandromf_199@hotmail.com>
2024-07-09 19:26:40 +0200

    v1.0.1 => Modulos y componentes para el listado de personajes de DBZ

--- src/app/heroes/components/hero/hero.component.ts
+++ src/app/heroes/components/hero/hero.component.ts
@@ -1,32 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-heroes-hero',
-  templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+	selector: 'app-heroes-hero',
+	templateUrl: './hero.component.html',
+	styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-    public name: string = 'ironman';
-    public age: number = 45;
+	public name: string = 'ironman';
+	public age: number = 45;
 
-    get capitalizedNamed(): string {
-        return this.name.toUpperCase();
-    }
+	get capitalizedNamed(): string {
+		return this.name.toUpperCase();
+	}
 
-    getHeroDescription(): string {
-        return `${this.name} - ${this.age} años`
-    }
+	getHeroDescription(): string {
+		return `${this.name} - ${this.age} años`;
+	}
 
-    changeHero(newHero: string):void {
-        this.name = newHero;
-    }
+	changeHero(newHero: string): void {
+		this.name = newHero;
+	}
 
-    changeAge(newAge: number):void {
-        this.age = newAge
-    }
+	changeAge(newAge: number): void {
+		this.age = newAge;
+	}
 
-    resetAll():void {
-        this.name = 'ironman';
-        this.age = 45;
-    }
+	resetAll(): void {
+		this.name = 'ironman';
+		this.age = 45;
+	}
 }
